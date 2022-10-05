26d93920de86cb6dec60a19c2dbe2ed18812bc91
Nate Wolfe <nate@wxlfe.dev>
2022-10-04 23:04:25 -0400

    Added Meta Description

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,6 +11,10 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
   <div>
     <Head>
       <title>{title}</title>
+      <meta
+        name='description'
+        content='A searchable list of Star Wars characters'
+      />
       <meta charSet='utf-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
     </Head>
